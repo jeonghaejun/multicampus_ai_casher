8c766884af26a932ddbd61fb7538513faa686be1
Bittah Swami <41178536+6democratickim9@users.noreply.github.com>
2021-05-12 00:00:01 +0900

    Update views.py
    conn수정

--- Cloud/cahser_server/views.py
+++ Cloud/cahser_server/views.py
@@ -7,13 +7,7 @@ import json
 
 # from .config import conn
 
-conn = {
-    "host": "multicampus.cx4rv1rxgkqv.ap-northeast-2.rds.amazonaws.com",
-    "port": 3306,
-    "user": "admin",
-    "password": "master123",
-    "database": "multicampus"
-}
+
 cursor = conn.cursor()
 
 
