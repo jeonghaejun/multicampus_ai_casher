093b5585d1a962fd739ae86a82be206c7d56f6ee
rewcho <rewcho@gmail.com>
2021-05-17 18:00:15 +0900

    android continued

--- IoT/present_list/app/src/main/java/com/example/present_list/ContactsDao.kt
+++ IoT/present_list/app/src/main/java/com/example/present_list/ContactsDao.kt
@@ -4,6 +4,8 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Dao
 interface ContactsDao {
@@ -16,4 +18,13 @@ interface ContactsDao {
     @Delete
     fun delete(contacts: Contacts)
 
+}
+
+object LoadData {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("3.35.202.158")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getload(): LoadData = retrofit.create(LoadData::class.java)
 }
--- IoT/present_list/app/src/main/java/com/example/present_list/MainActivity.kt
+++ IoT/present_list/app/src/main/java/com/example/present_list/MainActivity.kt
@@ -4,50 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_main.*
+
+val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    val presentsList = mutableListOf<Contacts>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        val adapter = MainAdapter(presentsList)
+        PresentlistView.adapter = adapter
 
-    val TAG = "ListActivity"
-    var db: AppDatabase? = null
-    var contactsList = mutableListOf<Contacts>()
+        // 아이템 클릭 및 취소
+        cancel_button.setOnClickListener {
+            override fun onClick(v: View, position: Int) {
+                val contacts = presentsList[position]
 
-    // 초기화
-    db = AppDatabase.getInstance(this)
+                presentsList.removeAt(position) // 취소, 리스트에서 삭제
+                adapter.notifyDataSetChanged()  // 리스트뷰 갱신
 
-    // 이전에 저장한 내용 모두 불러오기 + 추가
-    val savedContacts = db!!.contactsDao().getAll()
-    if(savedContacts.isNotEmpty()) {
-        contactsList.addAll(savedContacts)
-    }
-
-    // 아이템 클릭 및 취소
-    val adapter = ContactsListAdapter(contactsList)
-    adapter.setItemClickListener(object: ContactsListAdapter.OnItemClickListener{
-        override fun onClick(v: View, position, Int) {
-
-            val contacts = contactsList[position]
-
-            contactsList.removeAt(position) // 취소, 리스트에서 삭제
-            adapter.notifyDataSetChanged()  // 리스트뷰 갱신
-
-            Log.d(TAG, "remove item($position).name:${contacts.Product_name}")
+                Log.d(AppCompatActivity.TAG, "remove item($position).name:${contacts.Product_name}")
+            }
         }
-    })
-    mRecyclerView.adapter = adapter
+        PresentlistView.adapter = adapter
 
-
-
-// 결제 버튼 클릭 : 거래내역 추가
-    button.setOnClickListener {
-
-        // 거래내역 추가
-
+        // 결제 내역 추가
+        button.setOnClickListener {
+            val user_phonenum = ##
+            val DATE =
+        }
     }
-
-
 }
